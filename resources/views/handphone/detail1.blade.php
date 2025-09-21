b33f95b1eb60d5df77365f3091bbb1cc900aa3ee
Yasika-Widiyastuti <yasikawidiyastuti@gmail.com>
2025-09-21 09:32:32 +0700

    Update detail1.blade.php

--- resources/views/handphone/detail1.blade.php
+++ resources/views/handphone/detail1.blade.php
@@ -27,9 +27,9 @@
             Smartphone flagship dengan kamera jernih dan performa kencang. Kondisi sangat baik, baterai awet, layar AMOLED 120Hz.
           </p>
           <p class="text-2xl font-bold text-blue-600 mb-6">Rp 80.000 / hari</p>
-          <a href="/pembayaran" 
-             class="inline-block bg-blue-600 text-white font-semibold px-6 py-3 rounded-lg hover:bg-blue-700">
-             Sewa Sekarang
+            <a href="{{ route('checkout') }}" 
+               class="inline-block bg-blue-600 text-white font-semibold px-6 py-3 rounded-lg hover:bg-blue-700">
+               Sewa Sekarang
           </a>
         </div>
       </div>
